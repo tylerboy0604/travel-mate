5fffd35d1817298d8def5b3dbff6c25d9b4b4259
Sally <94455763+SallyAlsayed@users.noreply.github.com>
2023-10-22 18:24:54 +0200

    kt bestanden

---
+++ app/src/main/java/com/example/travel_mate/Boards.kt
@@ -0,0 +1,11 @@
+package com.example.travel_mate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Boards : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boards)
+    }
+}
