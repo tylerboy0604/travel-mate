61888d2eb98e8deb0b4e57e5fdfe02f4d5dc1bb2
jilk1 <94365522+jilk1@users.noreply.github.com>
2023-10-22 13:54:02 +0200

    button op de splashscreen neer gezet

--- app/src/main/java/com/example/travel_mate/MainActivity.kt
+++ app/src/main/java/com/example/travel_mate/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.travel_mate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var buttonhome: Button = findViewById(R.id.button)
+        buttonhome.setOnClickListener(){
+            gotohome();
+        }
+    }
+
+
+    fun gotohome(){
+        val activityhome = Intent(this, homepage::class.java);
+        startActivity(activityhome);
+
     }
 }
